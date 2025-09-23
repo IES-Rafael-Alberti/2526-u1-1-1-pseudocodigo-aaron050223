581f8c89d4576157a4dece1600de9e5d33d9c037
aaron050223 <agallardoc@iesrafaelalberti.es>
2025-09-23 10:46:14 +0200

    Edito los ejercicios 10 y 11

--- Ejercicios/ej10.py
+++ Ejercicios/ej10.py
@@ -2,4 +2,14 @@
 Suma de tres números usando solo dos variables
 '''
 
-# No se como se haria esta sin usar una tercera variable con funciones
+def pedirNumero():
+    return int(input("Número >> "))
+
+def mostrarResultado(resultado):
+    print(f"El resultado es >> {resultado}")
+
+def main():
+    numero = pedirNumero() + pedirNumero() + pedirNumero()
+    mostrarResultado(numero)
+if __name__ == "__main__":
+    main()
--- Ejercicios/ej11.py
+++ Ejercicios/ej11.py
@@ -2,13 +2,13 @@
 Suma de tres números sin variables adicionales
 '''
 
-def suma(numero1,numero2,numero3):
-    print(f"La suma de {numero1}, {numero2} y {numero3} es {numero1 + numero2 + numero3}")
+def pedirNumero():
+    return int(input("Número >> "))
+
+def mostrarResultado(resultado):
+    print(f"El resultado es >> {resultado}")
 
 def main():
-    numero1 = int(input("Número 1 >> "))
-    numero2 = int(input("Número 2 >> "))
-    numero3 = int(input("Número 3 >> "))
-    suma(numero1,numero2,numero3)
+    return mostrarResultado(pedirNumero() + pedirNumero() + pedirNumero())
 if __name__ == "__main__":
     main()
